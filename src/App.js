7ceb81fe1cda5d279302bace7fdf7d2ab85681be
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-11-15 12:53:50 +0530

    created pages and added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
-     hello
+     <Router>
+      <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/profile" element={<Profile/>}/>
+       <Route path="/sign-in" element={<SignIn/>}/>
+       <Route path="/sign-up" element={<SignUp/>}/>
+       <Route path="/forgot-password" element={<ForgotPassword/>}/>
+       <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+     </Router>
     </>
   );
 }
